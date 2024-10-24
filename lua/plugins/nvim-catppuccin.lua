7116676379d99f8f70d404a5981c72953bea6169
Gvln <santis.paez2414@outlook.com>
2024-10-24 15:50:07 -0500

    fix: solve some catppuccin colors problem

--- lua/plugins/nvim-catppuccin.lua
+++ lua/plugins/nvim-catppuccin.lua
@@ -1,23 +1,22 @@
 return {
   'catppuccin/nvim',
   lazy = false,
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup({
+  name = 'catppucin',
+	priority = 1000,
+	config = function()
+		require('catppuccin').setup({
 			transparent_background = true,
-      term_colors = true,
-      integrations = {
-        cmp = true,
+			integrations = {
+				cmp = true,
 				treesitter = true,
-        lsp_saga = true,
-        gitsigns = true,
-      },
-      styles = {
-        comments = { "italic" },
-        keywords = { "bold" },
-      },
-    })
+				lsp_saga = true,
+				gitsigns = true,
+			},
+			styles = {
+				comments = { "italic" },
+				keywords = { "bold" },
+			},
+		})
     vim.cmd.colorscheme 'catppuccin'
   end
 }
