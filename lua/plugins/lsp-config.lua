5797e2e8634071f91773cefbf8d0f2f3beea6054
Gvln-S <santis.paez2414@outlook.com>
2026-02-19 13:15:49 -0500

    fix: :bug: solve lsp for linux

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,10 +8,29 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      require("mason").setup({ ui = { border = "rounded" } })
+      require("mason").setup({ 
+        ui = { border = "rounded" } 
+      })
 
       local lspconfig = require("lspconfig")
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      vim.diagnostic.config({
+        virtual_text = { 
+          prefix = '●',
+          source = "always",
+        },
+        float = { 
+          source = "always",
+          border = "rounded"
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('UserLspConfig', {}),
@@ -23,23 +42,45 @@ return {
           vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
           vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
           vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+          local client = vim.lsp.get_client_by_id(ev.data.client_id)
+          if client and client.name ~= "null-ls" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
         end,
       })
-
       require("mason-lspconfig").setup({
-        ensure_installed = { "clangd", "lua_ls", "pyright" },
+        ensure_installed = { 
+          "jdtls", "lua_ls", "ts_ls", "html", "cssls", 
+          "kotlin_language_server", "bashls", "marksman", 
+          "pyright", "clangd" 
+        },
+        automatic_installation = true,
         handlers = {
           function(server_name)
-            lspconfig[server_name].setup({ capabilities = capabilities })
+            if server_name ~= "jdtls" then
+              lspconfig[server_name].setup({ capabilities = capabilities })
+            end
           end,
           ["clangd"] = function()
             local is_windows = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
             local cmd = is_windows and {
-              "C:/msys64/mingw64/bin/clangd.exe", "--background-index", "--clang-tidy",
+              "C:/msys64/mingw64/bin/clangd.exe",
+              "--background-index",
+              "--clang-tidy",
+              "--header-insertion=iwyu",
+              "--completion-style=detailed",
+              "--function-arg-placeholders",
+              "--fallback-style=llvm",
               "--query-driver=C:/msys64/mingw64/bin/clang++.exe"
             } or {
-              "clangd", "--background-index", "--clang-tidy", "--fallback-style=llvm"
+              "clangd",
+              "--background-index",
+              "--clang-tidy",
+              "--fallback-style=llvm"
             }
+
             lspconfig.clangd.setup({
               cmd = cmd,
               capabilities = capabilities,
@@ -48,5 +89,9 @@ return {
         }
       })
     end,
-  }
+  },
+  {
+    "mfussenegger/nvim-jdtls",
+    ft = "java",
+  },
 }
